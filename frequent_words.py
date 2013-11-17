206d8b4039145ac0314427ee2c6bb5006cc55765
Sky Yin <sky.yin@gmail.com>
2013-11-16 19:48:20 -0800

    fixed the input error in frequent_words and add doctest

--- frequent_words.py
+++ frequent_words.py
@@ -2,21 +2,30 @@
 
 import sys
 
-lines = open(sys.argv[1].strip(), 'r').readlines()
-text = str(lines[0]).strip()
-k = int(lines[1]).strip()
-l = len(text)
 
-d = {}
+def frequent_words(text, k):
+	"""
+	>>> frequent_words("ACGTTGCATGTCGCATGATGCATGAGAGCT", 4)
+	['CATG', 'GCAT']
+	"""
+	
+	l = len(text)
+	d = {}
 
-for i in range(l - k + 1):
-	kmer = text[i:i + k]
-	if kmer in d:
-		d[kmer] += 1
-	else:
-		d[kmer] = 1
+	for i in range(l - k + 1):
+		kmer = text[i:i + k]
+		if kmer in d:
+			d[kmer] += 1
+		else:
+			d[kmer] = 1
 
-max_frequency = max(d.values())
+	max_frequency = max(d.values())
+	return [kmer for kmer, freq in d.items() if freq == max_frequency]
 
-answer = [kmer for kmer, freq in d.items() if freq == max_frequency]
-print(" ".join(answer))
+
+if __name__ == "__main__":
+	lines = open(sys.argv[1].strip(), 'r').readlines()
+	text = str(lines[0].strip())
+	k = int(lines[1].strip())
+
+	print(" ".join(frequent_words(text, k)))
